5d460fa7b60da4f68a41649f97875a5fe15cbe71
ash89tiger <153449354+ash89tiger@users.noreply.github.com>
2023-12-17 16:56:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ $('button').click(function(){
 })
 
 $('button').eq(0).click(function(){
-    $('#header').css('background-image','url(/img/해파리연구소.jpeg)')
+    $('#header').css('background-image','url(./img/해파리연구소.jpeg)')
     $('.hero p1').text('신비한 해파리 연구소')
     $('.hero p2').text('말랑말랑 해파리의 비밀을 찾아보자')
 })
 
 $('button').eq(1).click(function(){
-    $('#header').css('background-image','url(/img/상어먹이.png)')
+    $('#header').css('background-image','url(./img/상어먹이.png)')
     $('.hero p1').text('스릴만점 상어 먹이주기')
     $('.hero p2').text('아슬아슬 스릴만점 상어 먹이주기 공연')
 })
 
 $('button').eq(2).click(function(){
-    $('#header').css('background-image','url(/img/인어공주.png)')
+    $('#header').css('background-image','url(./img/인어공주.png)')
     $('.hero p1').text('바다마법사와 인어공주 이야기')
     $('.hero p2').text('바다 마법사가 산호마을에 나타났다')
 })
 
 $('button').eq(3).click(function(){
-    $('#header').css('background-image','url(/img/수달먹이.png)')
+    $('#header').css('background-image','url(./img/수달먹이.png)')
     $('.hero p1').text('아기수달 먹이주기')
     $('.hero p2').text('앙증맞은 수달과 아쿠아리스트의 이야기')
 })
